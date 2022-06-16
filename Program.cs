5cdca38355ee12a4777525b2cf3cfaa04044aa68
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-16 17:27:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            Orders(product, quantity);
+        }
+        static void Orders(string product, int quantity)
+        {
+            if (product == "coffee")
+            {
+                Console.WriteLine($"{(quantity * 1.50):f2}");
+            }
+            else if (product == "water")
+            {
+                Console.WriteLine($"{(quantity * 1.00):f2}");
+            }
+            else if (product == "coke")
+            {
+                Console.WriteLine($"{(quantity * 1.40):f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{(quantity * 2.00):f2}");
+            }
+        }
+    }
+}
